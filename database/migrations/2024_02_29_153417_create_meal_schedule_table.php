aa2706f5ecdff3ce45444dc57ba8d4945f3390a8
Javlonbek <javlonrahmatullayev07@gmail.com>
2024-02-29 16:32:02 +0500

    Fill migration fields

---
+++ database/migrations/2024_02_29_153417_create_meal_schedule_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('meal_schedule', function (Blueprint $table) {
+            $table->id();
+            $table->integer('meal_id');
+            $table->date('date');
+            $table->integer('shift_id')->nullable();
+            $table->integer('created_by')->nullable();
+            $table->integer('updated_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('meal_schedule');
+    }
+};
